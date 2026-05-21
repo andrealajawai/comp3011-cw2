396192929c950d1e4d13e5d1df0ceeef2c021eb5
Andrea Philemon <andrealajawai@gmail.com>
2026-05-21 23:24:22 +0100

    added ranking to search results

--- src/search.py
+++ src/search.py
@@ -3,7 +3,10 @@ from src.indexer import tokenise
 
 def print_word(index, word):
     """Return the inverted index entry for one word."""
-    word = word.lower()
+    word = word.lower().strip()
+
+    if not word:
+        return "Please enter a word to print."
 
     if word not in index:
         return f"No results found for '{word}'."
@@ -17,15 +20,36 @@ def find_query(index, query):
 
     if not words:
         return "Please enter a search query."
-
-    if any(word not in index for word in words):
+    
+    missing_words = [word for word in words if word not in index]
+    if missing_words:
         return []
-
-    page_sets = []
-
-    for word in words:
-        page_sets.append(set(index[word].keys()))
-
+    
+    page_sets = [set(index[word].keys()) for word in words]
     matching_pages = set.intersection(*page_sets)
 
-    return sorted(matching_pages)
+    ranked_pages = []
+
+    for page in matching_pages:
+        score = 0
+
+        for word in words:
+            score += index[word][page]["frequency"]
+
+        ranked_pages.append((page, score))
+
+    ranked_pages.sort(key=lambda item: item[1], reverse=True)
+
+    #if any(word not in index for word in words):
+    #    return []
+
+    #page_sets = []
+
+    #for word in words:
+    #    page_sets.append(set(index[word].keys()))
+
+    #matching_pages = set.intersection(*page_sets)
+
+    #return sorted(matching_pages)
+
+    return ranked_pages
